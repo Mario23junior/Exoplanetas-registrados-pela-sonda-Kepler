690fae2e6c6fc78d7b272ff8df3f83602f9b41a6
Mario23junior <mariojunior3251@gmail.com>
2022-02-10 01:23:26 -0300

    Iniciando api de busca por exoplanetas

---
+++ src/test/java/com/v1/api/exoplanet/ExoplanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.api.exoplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoplanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
